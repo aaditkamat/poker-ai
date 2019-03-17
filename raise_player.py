5562699cc37821526e305dc0cff9c1f236652aa5
Aadit Kamat <aadit.k12@gmail.com>
2019-03-17 11:25:00 +0800

    Add class methods for evaluating certain hands
    Signed-off-by: Aadit Kamat <aadit.k12@gmail.com>

--- raise_player.py
+++ raise_player.py
@@ -1,31 +1,78 @@
 from pypokerengine.players import BasePokerPlayer
-from time import sleep
 import pprint
 
+
 class RaisedPlayer(BasePokerPlayer):
+    card_rank = ['A', 'K', 'Q', 'J'] + [str(num) for num in range(10, 1, -1)]
 
-  def declare_action(self, valid_actions, hole_card, round_state):
-    for i in valid_actions:
-        if i["action"] == "raise":
-            action = i["action"]
-            return action  # action returned here is sent to the poker engine
-    action = valid_actions[1]["action"]
-    return action # action returned here is sent to the poker engine
+    def __init__(self):
+      self.value_map = {}
+      self.suit_map = {}
 
-  def receive_game_start_message(self, game_info):
-    pass
+    def add_to_map(self, map, key):
+       if key in map:
+         map[key] += 1
+       else:
+         map[key] = 1
 
-  def receive_round_start_message(self, round_count, hole_card, seats):
-    pass
+    def group_by(self, cards, criteria):
+      if criteria == "suit":
+        for card in cards:
+          self.add_to_map(self.suit_map, card[0])
+      else:
+        for card in cards:
+          self.add_to_map(self.value_map, card[1:])
 
-  def receive_street_start_message(self, street, round_state):
-    pass
+    def pairs(self):
+        return len([self.value_map[key] == 2 for key in self.value_map])
 
-  def receive_game_update_message(self, action, round_state):
-    pass
+    def pair(self):
+        return self.same_kind(2) == 1
 
-  def receive_round_result_message(self, winners, hand_info, round_state):
-    pass
+    def two_pairs(self):
+        return self.same_kind(2) == 2
+
+    def three_of_a_kind(self):
+        return self.same_kind(3) == 1
+
+    def straight(self):
+        values = sorted(self.value_map.values(), c)
+
+    def same_kind(self, n):
+        return self.value_map.values().count(n)
+
+    def three_of_a_kind(self, cards):
+        return self.same_kind(cards, 3)
+
+    def four_of_a_kind(self, cards):
+        return self.same_kind(cards, 4)
+
+    def evaluation_function(self):
+        pass
+
+    def declare_action(self, valid_actions, hole_card, round_state):
+        for i in valid_actions:
+            if i["action"] == "raise":
+                action = i["action"]
+                return action  # action returned here is sent to the poker engine
+        action = valid_actions[1]["action"]
+        return action # action returned here is sent to the poker engine
+
+    def receive_game_start_message(self, game_info):
+        pass
+
+    def receive_round_start_message(self, round_count, hole_card, seats):
+      pass
+
+    def receive_street_start_message(self, street, round_state):
+      pass
+
+    def receive_game_update_message(self, action, round_state):
+      pass
+
+    def receive_round_result_message(self, winners, hand_info, round_state):
+      pass
+
 
 def setup_ai():
-  return RandomPlayer()
+  return RaisedPlayer()
